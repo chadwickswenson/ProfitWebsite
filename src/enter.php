5f608763d223475089f836991d9f96519a9342ed
chadwickswenson <chadswenson@chad-mac.local>
2013-11-30 19:30:59 -0800

    Intro Page Enter email

---
+++ src/enter.php
@@ -0,0 +1,19 @@
+<?php 
+ 
+		$con = mysql_connect("db454710182.db.1and1.com","dbo454710182","Oknbfr1360");
+    
+        if (!$con)
+        {
+            die('Could not connect: ' . mysql_error());
+        }
+       
+        mysql_select_db("db454710182", $con);
+        
+		$insert = "INSERT INTO db454710182.emails (email)
+					VALUES ('".$_POST['email']."')";
+		echo $insert;
+		mysql_query($insert);
+		
+		mysql_close($con);
+		
+		?>
